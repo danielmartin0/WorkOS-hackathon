d80e3be01aaed47db75a7c63d517175a86b63cc8
danielmartin0 <github@danielgmartin.com>
2026-03-07 12:54:30 -0800

    Update resource.lua

--- mod/prototypes/resource.lua
+++ mod/prototypes/resource.lua
@@ -1,14 +1,16 @@
 local merge = require("lib").merge
 
+local hackium_tint = { r = 0.35, g = 0.9, b = 1, a = 1 }
+
 data:extend({
 	{
 		type = "item",
 		name = "hackium",
 		icons = {
 			{
-				icon = "__space-age__/graphics/icons/bioflux.png",
+				icon = "__base__/graphics/icons/iron-plate.png",
 				icon_size = 64,
-				tint = { r = 0.35, g = 0.9, b = 1, a = 1 },
+				tint = hackium_tint,
 			},
 		},
 		subgroup = "raw-resource",
@@ -20,9 +22,9 @@ data:extend({
 		name = "hackium",
 		icons = {
 			{
-				icon = "__space-age__/graphics/icons/bioflux.png",
+				icon = "__base__/graphics/icons/iron-plate.png",
 				icon_size = 64,
-				tint = { r = 0.35, g = 0.9, b = 1, a = 1 },
+				tint = hackium_tint,
 			},
 		},
 		minable = {
@@ -31,5 +33,17 @@ data:extend({
 		},
 		autoplace = "nil",
 		map_color = { r = 0.45, g = 0.9, b = 1 },
+		stages = {
+			sheet = {
+				filename = "__base__/graphics/entity/iron-ore/iron-ore.png",
+				priority = "extra-high",
+				size = 128,
+				frame_count = 8,
+				variation_count = 8,
+				scale = 0.5,
+				tint = hackium_tint,
+			},
+		},
+		mining_visualisation_tint = hackium_tint,
 	}),
 })
